49e61b517509393aa760418dc59b4e933afb1a34
mabosaimi <132561114+MAbOsaimi@users.noreply.github.com>
2024-02-26 23:10:33 +0300

    Improve logic and styling

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const container = document.getElementById("container");
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.setAttribute("id", `square-x${i}-y${j}`);
-    square.classList.add("square");
-    container.appendChild(square);
+makeGrid(16, 16);
+function makeGrid(width, height) {
+  for (let i = 1; i <= height; i++) {
+    const squaresRow = document.createElement("div");
+    squaresRow.setAttribute("id", `squares-row-${i}`);
+    squaresRow.classList.add("squares-row");
+    for (let j = 1; j <= width; j++) {
+      const square = document.createElement("div");
+      square.setAttribute("id", `square-${j}`);
+      square.classList.add("square");
+      squaresRow.appendChild(square);
+    }
+    container.appendChild(squaresRow);
   }
 }
