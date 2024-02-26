a1645d9a853fc822300f30473aec3d8026a5f727
mabosaimi <132561114+MAbOsaimi@users.noreply.github.com>
2024-02-27 02:25:53 +0300

    Implement the full logic and enhance styling

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ makeGrid(16, 16);
 makeGridBtn.addEventListener("click", () => {
   let width = parseInt(prompt("Enter width"));
   width = width > 100 ? 100 : width;
+  width = width <= 0 || !width ? 16 : width;
   let height = parseInt(prompt("Enter height:"));
   height = height > 100 ? 100 : height;
+  height = height <= 0 || !height ? 16 : height;
   container.replaceChildren();
   makeGrid(width, height);
 });
@@ -21,8 +23,32 @@ function makeGrid(width, height) {
       const square = document.createElement("div");
       square.setAttribute("id", `square-${j}`);
       square.classList.add("square");
+      const red = Math.ceil(Math.random() * 255);
+      const blue = Math.ceil(Math.random() * 255);
+      const green = Math.ceil(Math.random() * 255);
+      square.style.setProperty(
+        "--square-background-color",
+        `rgb(${red}, ${blue}, ${green})`,
+      );
+      square.style.setProperty("--square-brightness", "110%");
       squaresRow.appendChild(square);
     }
+
     container.appendChild(squaresRow);
   }
+  const squares = document.querySelectorAll(".square");
+
+  squares.forEach((currentValue) => {
+    currentValue.addEventListener("mouseover", () => {
+      const brightness =
+        parseInt(currentValue.style.getPropertyValue("--square-brightness")) -
+          10 <
+        0
+          ? 0
+          : parseInt(
+              currentValue.style.getPropertyValue("--square-brightness"),
+            ) - 10;
+      currentValue.style.setProperty("--square-brightness", `${brightness}%`);
+    });
+  });
 }
