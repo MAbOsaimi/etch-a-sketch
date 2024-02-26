c71097886031ca4bb43e3b8be2b9d4b2b3c5c4dc
mabosaimi <132561114+MAbOsaimi@users.noreply.github.com>
2024-02-26 23:40:45 +0300

    Give user ability to specify width and height

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const container = document.getElementById("container");
+const makeGridBtn = document.getElementById("make-grid-btn");
+
 makeGrid(16, 16);
+
+makeGridBtn.addEventListener("click", () => {
+  let width = parseInt(prompt("Enter width"));
+  width = width > 100 ? 100 : width;
+  let height = parseInt(prompt("Enter height:"));
+  height = height > 100 ? 100 : height;
+  container.replaceChildren();
+  makeGrid(width, height);
+});
+
 function makeGrid(width, height) {
   for (let i = 1; i <= height; i++) {
     const squaresRow = document.createElement("div");
